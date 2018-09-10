e549a8f1b4e940cafc1df437e78e1080faed6923
topitzik@gmail.com <GHtimp2ma1>
2018-09-10 20:33:08 +0300

    example read and write to DB

--- app/src/main/java/com/example/foxtz/gameplan/PostsActivity.java
+++ app/src/main/java/com/example/foxtz/gameplan/PostsActivity.java
@@ -13,8 +13,11 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PostsActivity extends AppCompatActivity {
 
@@ -68,6 +71,23 @@ public class PostsActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("users/"+user.getUid());
         DatabaseReference myName = myRef.child("name");
         myName.setValue("Ali G");
+
+        myName.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("read DB", "Value is: " + value);
+                mTextMessage.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("read DB", "Failed to read value.", error.toException());
+            }
+        });
     }
 
 }
