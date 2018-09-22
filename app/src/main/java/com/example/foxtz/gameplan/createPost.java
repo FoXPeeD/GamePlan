0c0b35f505c3c88b324a88214d0c1733ae00ff67
taltz <taltzafrir2@gmail.com>
2018-09-22 19:46:07 +0300

    added date spinners

--- app/src/main/java/com/example/foxtz/gameplan/createPost.java
+++ app/src/main/java/com/example/foxtz/gameplan/createPost.java
@@ -95,7 +95,7 @@ public class createPost extends AppCompatActivity {
         daysAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         daySpinner.setAdapter(daysAdapter);
 
-        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        monthSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch(position) {
