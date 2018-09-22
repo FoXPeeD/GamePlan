30aeabdffcf9907648a235f83ba141615e864aaf
taltz <taltzafrir2@gmail.com>
2018-09-22 18:04:57 +0300

    added category and game spinners

--- app/src/main/java/com/example/foxtz/gameplan/createPost.java
+++ app/src/main/java/com/example/foxtz/gameplan/createPost.java
@@ -15,27 +15,31 @@ public class createPost extends AppCompatActivity {
         setContentView(R.layout.activity_create_post);
         getSupportActionBar().setTitle("New Post");
 
+        //catogory spinner init
         Spinner categorySpinner = findViewById(R.id.categorySpinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(this,
                 R.array.category_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
         categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
         categorySpinner.setAdapter(categoryAdapter);
 
+        //game spinner init
         final Spinner gameSpinner = findViewById(R.id.gameSpinner);
-        gameSpinner.getSelectedView().setEnabled(false);
-        gameSpinner.setEnabled(false);
+
+        ArrayAdapter<CharSequence> gameAdapter = ArrayAdapter.createFromResource(createPost.this,
+                R.array.ball_games_array, android.R.layout.simple_spinner_item);
+        gameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gameSpinner.setAdapter(gameAdapter);
+//        gameSpinner.getSelectedView().setEnabled(false);
+//        gameSpinner.setEnabled(false);
 
 
         categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                gameSpinner.getSelectedView().setEnabled(true);
-                gameSpinner.setEnabled(true);
-                ArrayAdapter<CharSequence> gameAdapter = ArrayAdapter.createFromResource(createPost.this,
-                        R.array.ball_games_array, android.R.layout.simple_spinner_item);
+//                gameSpinner.getSelectedView().setEnabled(true);
+//                gameSpinner.setEnabled(true);
+//                ArrayAdapter<CharSequence> gameAdapter = ArrayAdapter.createFromResource(createPost.this,
+//                        R.array.ball_games_array, android.R.layout.simple_spinner_item);
 
 //                switch(position) {
 //                    case 0:
@@ -60,8 +64,8 @@ public class createPost extends AppCompatActivity {
 //                }
 
 
-                gameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                gameSpinner.setAdapter(gameAdapter);
+//                gameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//                gameSpinner.setAdapter(gameAdapter);
             }
 
             @Override
