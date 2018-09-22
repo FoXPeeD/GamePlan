4b31f722758382dfbf34d5c719e9089e11a1e265
taltz <taltzafrir2@gmail.com>
2018-09-22 16:55:57 +0300

    finished create post activity layout

--- app/src/main/java/com/example/foxtz/gameplan/createPost.java
+++ app/src/main/java/com/example/foxtz/gameplan/createPost.java
@@ -2,6 +2,8 @@ package com.example.foxtz.gameplan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class createPost extends AppCompatActivity {
 
@@ -10,6 +12,15 @@ public class createPost extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
         getSupportActionBar().setTitle("New Post");
+
+        Spinner spinner = findViewById(R.id.categorySpinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.category_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
     }
 }
