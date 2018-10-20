e2f97165f73d0b789cc08b06c23c684bd330e3cb
taltz <taltzafrir2@gmail.com>
2018-10-20 18:39:34 +0300

    city spinner

--- app/src/main/java/com/example/foxtz/gameplan/registerActivity.java
+++ app/src/main/java/com/example/foxtz/gameplan/registerActivity.java
@@ -1,13 +1,19 @@
 package com.example.foxtz.gameplan;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,6 +23,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class registerActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -33,7 +43,77 @@ public class registerActivity extends AppCompatActivity {
         final EditText email = findViewById(R.id.emailEditText);
         final EditText password = findViewById(R.id.passwordEditText);
         final EditText age = findViewById(R.id.ageEditText);
-        final EditText city = findViewById(R.id.cityEditText);
+        final Spinner citySpinner = findViewById(R.id.citySpinner);
+
+        // Initializing a String Array
+        String[] cities = new String[]{
+                "Select city",
+                "Haifa",
+                "Tel-Aviv",
+                "Jerusalem",
+                "Netanya",
+                "Beer-sheva",
+                "Eilat",
+                "Nahariya",
+                "Qiryat-shemona"
+        };
+
+        final List<String> citiesList = new ArrayList<>(Arrays.asList(cities));
+
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.spinner_item, citiesList){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        citySpinner.setAdapter(spinnerArrayAdapter);
+
+        citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if(position > 0){
+                    // Notify the selected item text
+                    Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         mAuth = FirebaseAuth.getInstance();
 
         signUp.setOnClickListener(new View.OnClickListener() {
@@ -55,11 +135,11 @@ public class registerActivity extends AppCompatActivity {
                                     DatabaseReference myRef = database.getReference("users/"+user.getUid());
                                     myRef.child("user name").setValue(userName.getText().toString());
                                     myRef.child("age").setValue(age.getText().toString());
-                                    myRef.child("city").setValue(city.getText().toString());
+                                    myRef.child("city").setValue(citySpinner.getItemAtPosition(citySpinner.getSelectedItemPosition()).toString());
                                     //verify all text fields are not empty
                                     if (userName.getText().toString().matches("") ||
                                             age.getText().toString().matches("") ||
-                                            city.getText().toString().matches("")){
+                                            citySpinner.getItemAtPosition(citySpinner.getSelectedItemPosition()).toString().matches("Select city")){
                                         Toast.makeText(registerActivity.this, "Some of the fields are empty",
                                                 Toast.LENGTH_LONG).show();
                                         return;
