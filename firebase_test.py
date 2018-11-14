3ca5292e73a6739a22bb9efef9b56a702b10e483
topitzik@gmail.com <GHtimp2ma1>
2018-11-14 22:30:44 +0200

    solved python script running 6 times

--- firebase_test.py
+++ firebase_test.py
@@ -1,11 +1,27 @@
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://gameplan-1312c.firebaseio.com/', None)
-users = firebase.get('users', None)
-#print users.get('CEphnEaDLuWnjgQUsuSj40AutoC3')
+import sys
+def main(argv):
 
-usersDict = {}
-for userId in users.keys():
-	#usersDict[userId] = {}
-	#usersDict[userId]['age'] = userData['age']
-	print userId
-	print users[userId]
+	from firebase import firebase
+	firebase = firebase.FirebaseApplication('https://gameplan-1312c.firebaseio.com/', None)
+	users = firebase.get('users', None)
+	#print users.get('CEphnEaDLuWnjgQUsuSj40AutoC3')
+	usersDict = {}
+	for userId,userData in users.items():
+		#usersDict[userId] = {}
+		#usersDict[userId]['age'] = userData['age']
+		print userId
+		#print users[userId]
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main(sys.argv)
+
