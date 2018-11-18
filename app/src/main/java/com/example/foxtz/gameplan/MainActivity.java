b26ec2e503841bbea4af64f81de1f7ea797d7827
taltz <taltzafrir2@gmail.com>
2018-11-18 22:22:19 +0200

    added icons

--- app/src/main/java/com/example/foxtz/gameplan/MainActivity.java
+++ app/src/main/java/com/example/foxtz/gameplan/MainActivity.java
@@ -43,23 +43,8 @@ public class MainActivity extends AppCompatActivity {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d(TAG, "signInWithEmail:success");
-//                                    Toast.makeText(MainActivity.this, "Authentication is failed.",
-//                                            Toast.LENGTH_SHORT).show();
-                                    FirebaseUser user = mAuth.getCurrentUser();
 
-                                    //get id
-//                                    user.getIdToken(true).addOnSuccessListener(new OnSuccessListener<GetTokenResult>() {
-//                                        @Override
-//                                        public void onSuccess(GetTokenResult result) {
-//                                            String idToken = result.getToken();
-//                                            //Do whatever
-//                                            Log.d(TAG, "GetTokenResult result = " + idToken);
-////                                            Toast.makeText(MainActivity.this, "id is " + idToken,
-////                                                    Toast.LENGTH_SHORT).show();
-//                                        }
-//                                    });
-//                                    Toast.makeText(MainActivity.this, "id is " + user.getUid(),
-//                                                    Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
 
                                     Intent intent = new Intent(MainActivity.this, PostsActivity.class);
                                     intent.putExtra("key", "value");
